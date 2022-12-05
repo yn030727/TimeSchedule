707ee7c43cdc75e228dd5bc0e865d9375a351659
yn030727 <2601098388@qq.com>
2022-12-05 21:27:11 +0800

    1.Day1

---
+++ module_individual/src/main/java/com/example/module_individual/IndividualActivity.java
@@ -0,0 +1,14 @@
+package com.example.module_individual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndividualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual);
+    }
+}
