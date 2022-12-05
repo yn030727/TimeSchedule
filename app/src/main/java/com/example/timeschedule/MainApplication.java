33fdc8b67573b4c7b6879fb406777aae89d5884d
yn030727 <2601098388@qq.com>
2022-12-05 21:37:28 +0800

    1.Day1

---
+++ app/src/main/java/com/example/timeschedule/MainApplication.java
@@ -0,0 +1,29 @@
+package com.example.timeschedule;
+
+import static com.alibaba.android.arouter.launcher.ARouter.init;
+
+import android.app.Application;
+import android.os.Build;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(isDebug()){
+            ARouter.openLog();
+        }
+        init(this);
+    }
+
+    private boolean isDebug(){
+        return BuildConfig.DEBUG;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ARouter.getInstance().destroy();//关闭ARouter
+    }
+}
