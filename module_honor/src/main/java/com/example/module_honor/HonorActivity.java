4e6ecad3570e6f2903497e479fb7505a53aa13c9
yn030727 <2601098388@qq.com>
2022-12-09 13:32:36 +0800

    1.Day2

---
+++ module_honor/src/main/java/com/example/module_honor/HonorActivity.java
@@ -0,0 +1,17 @@
+package com.example.module_honor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.module_honor.R;
+
+
+public class HonorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_honor);
+    }
+}
