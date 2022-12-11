b953f5fcf730d2e18e092b31205fb28beee2b6cd
yn030727 <2601098388@qq.com>
2022-12-11 19:41:58 +0800

    1.Day4

---
+++ module_individual/src/main/java/com/example/module_individual/fragments/IndividualFragment.java
@@ -0,0 +1,23 @@
+package com.example.module_individual.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.example.module_individual.R;
+
+@Route(path = "/individual/IndividualFragment")
+public class IndividualFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_individual,container,false);
+        return view;
+    }
+}
--- module_main/src/main/java/com/example/module_main/ui/MainActivity.java
+++ module_main/src/main/java/com/example/module_main/ui/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.module_main.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -10,23 +14,33 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.module_main.R;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//这是主模块的第一界面
+// 功能:
+// 1. 初始化其他界面提供的Fragment
+// 2. 初始化主界面底部导航栏，以及按钮点击跳转界面
+// 代码目录
+// 0.声明全局变量
+// 1.设置瘦金体
+// 2.初始化变量
+// 3.初始化点击事件
 
 @Route(path="/main/MainActivity")
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //APP层需要声明的数据
-    Button app_navigation_schedule_btn;
-    Button app_navigation_challenge_btn;
-    Button app_navigation_social_btn;
-    Button app_navigation_person_btn;
-    TextView app_navigation_schedule_text;
+    Button app_navigation_schedule_btn;//底部导航栏的石以砥焉界面跳转按钮
+    Button app_navigation_challenge_btn;//底部导航栏的暂留芳华界面跳转按钮
+    Button app_navigation_social_btn;//底部导航栏的一觞一咏界面跳转按钮
+    Button app_navigation_person_btn;//底部导航栏的舍间陋室界面跳转按钮
+    TextView app_navigation_schedule_text;//和按钮配套的文本
     TextView app_navigation_challenge_text;
     TextView app_navigation_social_text;
     TextView app_navigation_person_text;
+    ArrayList<Fragment> fragmentArrayList; //存放各组件Fragment的集合
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +54,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView.setTypeface(typeface);*/
 
         //2.初始化变量
-        app_navigation_challenge_btn = findViewById(R.id.app_navigation_challenge_btn);//底部导航栏的石以砥焉界面跳转按钮
-        app_navigation_schedule_btn = findViewById(R.id.app_navigation_schedule_btn);//底部导航栏的暂留芳华界面跳转按钮
-        app_navigation_social_btn = findViewById(R.id.app_navigation_social_btn);//底部导航栏的一觞一咏界面跳转按钮
-        app_navigation_person_btn = findViewById(R.id.app_navigation_person_btn);//底部导航栏的舍间陋室界面跳转按钮
+        app_navigation_challenge_btn = findViewById(R.id.app_navigation_challenge_btn);
+        app_navigation_schedule_btn = findViewById(R.id.app_navigation_schedule_btn);
+        app_navigation_social_btn = findViewById(R.id.app_navigation_social_btn);
+        app_navigation_person_btn = findViewById(R.id.app_navigation_person_btn);
         app_navigation_schedule_text = findViewById(R.id.app_navigation_schedule_text);
         app_navigation_challenge_text = findViewById(R.id.app_navigation_challenge_text);
         app_navigation_social_text = findViewById(R.id.app_navigation_social_text);
         app_navigation_person_text = findViewById(R.id.app_navigation_person_text);
+
+
 
         //3.初始化按钮的点击事件
         app_navigation_challenge_btn.setOnClickListener(this);
@@ -55,7 +71,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         app_navigation_social_btn.setOnClickListener(this);
         app_navigation_person_btn.setOnClickListener(this);
 
-
+        //初始化Fragment
+        replaceFragment( (Fragment) ARouter.getInstance().build("/calendar/CalendarFragment").navigation());
+        onClick(app_navigation_schedule_btn);
     }
 
     @Override
@@ -65,18 +83,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             setEnable(app_navigation_challenge_btn);
             initText();
             app_navigation_challenge_text.setTextColor(Color.rgb(111, 24, 32));
+            replaceFragment((Fragment) ARouter.getInstance().build("/challenge/ChallengeFragment").navigation());
         } else if (id == R.id.app_navigation_schedule_btn) {
             setEnable(app_navigation_schedule_btn);
             initText();
             app_navigation_schedule_text.setTextColor(Color.rgb(111, 24, 32));
+            replaceFragment((Fragment) ARouter.getInstance().build("/calendar/CalendarFragment").navigation());
         } else if (id == R.id.app_navigation_social_btn) {
             setEnable(app_navigation_social_btn);
             initText();
             app_navigation_social_text.setTextColor(Color.rgb(111, 24, 32));
+            replaceFragment((Fragment) ARouter.getInstance().build("/socializing/SocializingFragment").navigation());
         } else if (id == R.id.app_navigation_person_btn) {
             setEnable(app_navigation_person_btn);
             initText();
             app_navigation_person_text.setTextColor(Color.rgb(111, 24, 32));
+            replaceFragment((Fragment) ARouter.getInstance().build("/individual/IndividualFragment").navigation());
         }
     }
 
@@ -94,10 +116,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         btn.setEnabled(false);
     }
+
     private void initText(){
         app_navigation_challenge_text.setTextColor(Color.rgb(181,181,181));
         app_navigation_schedule_text.setTextColor(Color.rgb(181,181,181));
         app_navigation_social_text.setTextColor(Color.rgb(181,181,181));
         app_navigation_person_text.setTextColor(Color.rgb(181,181,181));
+    }
+
+    //点击切换Fragment的方法
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_maininterface_container,fragment);
+        fragmentTransaction.commit();
+    }
+
+
+    //初始化其他界面提供的Fragment
+    private void initFragments(){
+        fragmentArrayList = new ArrayList<>();
+        Fragment challengeFragment = (Fragment) ARouter.getInstance().build("/challenge/ChallengeFragment").navigation();
+        Fragment calendarFragment = (Fragment) ARouter.getInstance().build("/calendar/CalendarFragment").navigation();
+        Fragment socializingFragment = (Fragment) ARouter.getInstance().build("/socializing/SocializingFragment").navigation();
+        Fragment individualFragment = (Fragment) ARouter.getInstance().build("/individual/IndividualFragment").navigation();
+        fragmentArrayList.add(challengeFragment);
+        fragmentArrayList.add(calendarFragment);
+        fragmentArrayList.add(socializingFragment);
+        fragmentArrayList.add(individualFragment);
     }
 }
