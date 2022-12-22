47eb940429e1afe3cfb41c8b8d46d7ca28417d78
yn030727 <2601098388@qq.com>
2022-12-22 21:53:01 +0800

    1.Day9

--- module_challenge/src/main/java/com/example/module_challenge/fragments/ChallengeFragment_FirstCard.java
+++ module_challenge/src/main/java/com/example/module_challenge/fragments/ChallengeFragment_FirstCard.java
@@ -1,9 +1,11 @@
 package com.example.module_challenge.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +16,20 @@ import com.example.module_challenge.R;
 
 @Route(path = "/challenge/ChallengeFragment_FirstCard")
 public class ChallengeFragment_FirstCard extends Fragment {
+    TextView challenge_firstCard_saying;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first_card,container,false);
+        //设置瘦金体
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/main_font_shoujin.ttf");
+
+
+        challenge_firstCard_saying = view.findViewById(R.id.challenge_firstcard_saying);
+        challenge_firstCard_saying.setTypeface(typeface);
+
+
         return view;
     }
 }
