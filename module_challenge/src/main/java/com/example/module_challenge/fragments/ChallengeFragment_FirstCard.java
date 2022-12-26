207d2ed2df7950b15db869c25efec96f9a8e1621
yn030727 <2601098388@qq.com>
2022-12-26 17:38:10 +0800

    1.Day12

--- module_challenge/src/main/java/com/example/module_challenge/fragments/ChallengeFragment_FirstCard.java
+++ module_challenge/src/main/java/com/example/module_challenge/fragments/ChallengeFragment_FirstCard.java
@@ -21,6 +21,14 @@ import org.greenrobot.eventbus.EventBus;
 
 import eventbus.EventChallenge_CardActivity_Back;
 
+//这是第一个挑战的详细介绍界面(之后几个与其类似，将不会有功能导图)
+//功能及实现：
+//代码目录：
+//   0.初始化Fragment的相关操作
+//   1.back图片和接下挑战按钮的功能介绍
+
+
+
 @Route(path = "/challenge/ChallengeFragment_FirstCard")
 public class ChallengeFragment_FirstCard extends Fragment implements View.OnClickListener {
     TextView challenge_firstCard_saying;
@@ -28,8 +36,10 @@ public class ChallengeFragment_FirstCard extends Fragment implements View.OnClic
     Button challenge_firstCard_button;
     //当前挑战项目是否被挑战，如果已经点击了同意挑战，那么就将这个值变为true
     //当挑战结束后，再将这个值重新变为false
-    Boolean challenge_firstCard_status = false;
+    Boolean challenge_firstCard_status = false; //当前是否正在挑战中（关于青史界面是需要已经完成一次挑战的信息，不用写在这里）
 
+
+    //0.初始化Fragment的相关操作
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -49,6 +59,10 @@ public class ChallengeFragment_FirstCard extends Fragment implements View.OnClic
         return view;
     }
 
+
+
+
+    //1.两个相关的点击事件
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.challenge_firstCard_back) {
@@ -57,6 +71,9 @@ public class ChallengeFragment_FirstCard extends Fragment implements View.OnClic
         }else if(v.getId() == R.id.challenge_firstCard_button){
             challenge_firstCard_status = true;
             Log.d("Ning_Challenge", "firstCard : " + challenge_firstCard_status);
+            //点击之后要更新数据库里面的内容
+            //先查看数据库里面的当前信息状况： 1.没有 说明第一次挑战，那么添加到数据库中  2.false 已经完成过一次或多次该挑战，变为true  3.true  正在进行挑战当中，无需进行接下来的操作
+
         }
     }
 }
