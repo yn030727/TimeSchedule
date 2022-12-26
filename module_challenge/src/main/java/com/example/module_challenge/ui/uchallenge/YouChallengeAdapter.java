278a5696371141bb354e19079eb52015131d6ce9
yn030727 <2601098388@qq.com>
2022-12-26 19:20:04 +0800

    1.Day12

--- module_challenge/src/main/java/com/example/module_challenge/ui/uchallenge/YouChallengeAdapter.java
+++ module_challenge/src/main/java/com/example/module_challenge/ui/uchallenge/YouChallengeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.module_challenge.ui.uchallenge;
 
+import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -8,15 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.module_challenge.logic.model.YouChallengeCard;
 
+import java.util.List;
+
 //这是已经接下挑战的适配器
 public class YouChallengeAdapter extends RecyclerView.Adapter<YouChallengeViewHolder>{
 
-
-    public YouChallengeAdapter(){}
+    List<YouChallengeCard> youChallengeCardList;
+    Typeface typeface;
+    public YouChallengeAdapter(List<YouChallengeCard> youChallengeCardList , Typeface typeface){
+        this.youChallengeCardList = youChallengeCardList;
+        this.typeface = typeface;
+    }
 
     @NonNull
     @Override
     public YouChallengeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         return null;
     }
 
@@ -27,7 +35,7 @@ public class YouChallengeAdapter extends RecyclerView.Adapter<YouChallengeViewHo
 
     @Override
     public int getItemCount() {
-        return 0;
+        return youChallengeCardList.size();
     }
 }
 class YouChallengeViewHolder extends RecyclerView.ViewHolder{
