96e407023ee4cea3a5cbd7bf3f960875cc1eb9f9
yn030727 <2601098388@qq.com>
2023-01-06 21:38:24 +0800

    1.Day14

--- module_honor/src/main/java/com/example/module_honor/fragments/HonorFragment.java
+++ module_honor/src/main/java/com/example/module_honor/fragments/HonorFragment.java
@@ -1,9 +1,12 @@
 package com.example.module_honor.fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +15,38 @@ import androidx.fragment.app.Fragment;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.module_honor.R;
 
+
+//第三界面:石以砥焉-青史界面
+//两个主要信息得数据统计
+//功能:
+
+//代码目录:
+//  0.声明变量
+//  1.点击事件
 @Route(path = "/honor/HonorFragment")
 public class HonorFragment extends Fragment {
+    //0.声明变量
+
+    TextView honor_fragment_textview1;
+    TextView honor_fragment_share_text;
+    TextView honor_fragment_consecutiveDay_text;
+
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_honor,container,false);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/main_font_shoujin.ttf");
+
+        //变量实例化
+        honor_fragment_textview1 = view.findViewById(R.id.honor_fragment_textview1);
+        honor_fragment_share_text = view.findViewById(R.id.honor_fragment_share_text);
+        honor_fragment_consecutiveDay_text = view.findViewById(R.id.honor_fragment_consecutiveDay_text);
+        honor_fragment_share_text.setTypeface(typeface);
+        honor_fragment_textview1.setTypeface(typeface);
+        honor_fragment_consecutiveDay_text.setTypeface(typeface);
+
+
         return view;
     }
 }
