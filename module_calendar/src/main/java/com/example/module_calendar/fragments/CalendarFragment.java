c732e8a4631b7a9bb5ccc50432e540d2b2f39b3b
yn030727 <2601098388@qq.com>
2023-01-17 20:33:45 +0800

    1.Day17

--- module_calendar/src/main/java/com/example/module_calendar/fragments/CalendarFragment.java
+++ module_calendar/src/main/java/com/example/module_calendar/fragments/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.example.module_calendar.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +12,17 @@ import androidx.fragment.app.Fragment;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.module_calendar.R;
+import com.haibin.calendarview.CalendarView;
 
 @Route(path = "/calendar/CalendarFragment")
 public class CalendarFragment extends Fragment {
+    CalendarView calendar_title_calendarView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar,container,false);
+        calendar_title_calendarView = view.findViewById(R.id.calendar_title_calendarView);
+
         return view;
     }
 }
