c9c75c4dbd4cc265569791ae8542cc8aba6887a2
yn030727 <2601098388@qq.com>
2023-01-20 17:45:01 +0800

    1.Day18

--- module_calendar/src/main/java/com/example/module_calendar/fragments/CalendarFragment.java
+++ module_calendar/src/main/java/com/example/module_calendar/fragments/CalendarFragment.java
@@ -1,28 +1,158 @@
 package com.example.module_calendar.fragments;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.module_calendar.R;
+import com.example.module_calendar.model.Article;
+import com.example.module_calendar.ui.BaseActivity;
+import com.example.module_calendar.ui.GroupItemDecoration;
+import com.example.module_calendar.ui.GroupRecyclerView;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarLayout;
 import com.haibin.calendarview.CalendarView;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 @Route(path = "/calendar/CalendarFragment")
-public class CalendarFragment extends Fragment {
-    CalendarView calendar_title_calendarView;
+public class CalendarFragment extends Fragment implements
+        CalendarView.OnCalendarSelectListener ,
+        CalendarView.OnYearChangeListener,
+        View.OnClickListener{
+
+
+    TextView mTextMonthDay;
+
+    TextView mTextYear;
+
+    TextView mTextLunar;
+
+    TextView mTextCurrentDay;
+
+    CalendarView mCalendarView;
+
+    RelativeLayout mRelativeTool;
+    private int mYear;
+    CalendarLayout mCalendarLayout;
+    GroupRecyclerView mRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar,container,false);
-        calendar_title_calendarView = view.findViewById(R.id.calendar_title_calendarView);
+        mTextMonthDay = view.findViewById(R.id.tv_month_day);
+        mTextYear = view.findViewById(R.id.tv_year);
+        mTextLunar = view.findViewById(R.id.tv_lunar);
+        mRelativeTool = view.findViewById(R.id.rl_tool);
+        mCalendarView = view.findViewById(R.id.calendarView);
 
+        mTextCurrentDay = view.findViewById(R.id.tv_current_day);
+        //点击月份切换到年份选择界面
+        mTextMonthDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!mCalendarLayout.isExpand()) {
+                    mCalendarLayout.expand();
+                    return;
+                }
+
+                mCalendarView.showYearSelectLayout(mYear);
+
+                mTextLunar.setVisibility(View.GONE);
+                mTextYear.setVisibility(View.GONE);
+                mTextMonthDay.setText(String.valueOf(mYear));
+            }
+        });
+
+
+        view.findViewById(R.id.fl_current).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarView.scrollToCurrent();
+            }
+        });
+        mCalendarLayout = view.findViewById(R.id.calendarLayout);
+        mCalendarView.setOnCalendarSelectListener(this);
+        mCalendarView.setOnYearChangeListener(this);
+        mTextYear.setText(String.valueOf(mCalendarView.getCurYear()));
+        mYear = mCalendarView.getCurYear();
+        mTextMonthDay.setText(mCalendarView.getCurMonth() + "月" + mCalendarView.getCurDay() + "日");
+        mTextLunar.setText("今日");
+        mTextCurrentDay.setText(String.valueOf(mCalendarView.getCurDay()));
+
+        int year = mCalendarView.getCurYear();
+        int month = mCalendarView.getCurMonth();
+
+
+
+        mRecyclerView = view.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.addItemDecoration(new GroupItemDecoration<String, Article>());
+        //mRecyclerView.setAdapter(new ArticleAdapter(this));
+        mRecyclerView.notifyDataSetChanged();
         return view;
     }
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        calendar.addScheme(new Calendar.Scheme());
+
+        return calendar;
+    }
+
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) {
+
+    }
+
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+        mTextLunar.setVisibility(View.VISIBLE);
+        mTextYear.setVisibility(View.VISIBLE);
+        mTextMonthDay.setText(calendar.getMonth() + "月" + calendar.getDay() + "日");
+        mTextYear.setText(String.valueOf(calendar.getYear()));
+        mTextLunar.setText(calendar.getLunar());
+        mYear = calendar.getYear();
+    }
+
+    @Override
+    public void onYearChange(int year) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
+
+
+
+
+
+
+
 }
---
+++ module_calendar/src/main/java/com/example/module_calendar/ui/ArticleAdapter.java
@@ -0,0 +1,4 @@
+package com.example.module_calendar.ui;
+
+public class ArticleAdapter {
+}
