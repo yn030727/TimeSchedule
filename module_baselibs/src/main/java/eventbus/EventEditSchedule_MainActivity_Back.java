05c3cedbc71df73a4469a8ad97bf7bb29e000504
yn030727 <2601098388@qq.com>
2023-01-31 23:20:05 +0800

    1.Day21

---
+++ module_baselibs/src/main/java/eventbus/EventEditSchedule_MainActivity_Back.java
@@ -0,0 +1,7 @@
+package eventbus;
+
+
+//事件
+//编辑计划界面的back返回按键
+public class EventEditSchedule_MainActivity_Back {
+}
--- module_editscheudle/src/main/java/com/example/module_editscheudle/EditScheduleFragment.java
+++ module_editscheudle/src/main/java/com/example/module_editscheudle/EditScheduleFragment.java
@@ -1,9 +1,13 @@
 package com.example.module_editscheudle;
 
+import android.annotation.SuppressLint;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +15,40 @@ import androidx.fragment.app.Fragment;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
+import org.greenrobot.eventbus.EventBus;
+
+import eventbus.EventChallenge_CardActivity_Back;
+
 
 @Route(path = "/editschedule/editschedulefragment")
-public class EditScheduleFragment extends Fragment {
+public class EditScheduleFragment extends Fragment implements View.OnClickListener {
+    TextView edit_schedule_create_textView;
+    TextView editschedule_cancel_textView;
+
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.editschedule_fragment , container , false);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/main_font_shoujin.ttf");
+        edit_schedule_create_textView = view.findViewById(R.id.editschedule_create_textView);
+        editschedule_cancel_textView = view.findViewById(R.id.editschedule_cancel_textView);
+
+        edit_schedule_create_textView.setTypeface(typeface);
+        editschedule_cancel_textView.setTypeface(typeface);
+        editschedule_cancel_textView.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        editschedule_cancel_textView.setOnClickListener(this);
         return view;
+    }
+
+
+
+    //1. 点击事件
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.editschedule_cancel_textView){
+            //点击取消界面
+        }
     }
 }
