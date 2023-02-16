d40933ed7257ba9aef4b7d91c94f26eeaa223f4a
yn030727 <2601098388@qq.com>
2023-02-16 18:31:30 +0800

    1.Day24

---
+++ module_baselibs/src/main/java/eventbus/EventSchedule.java
@@ -0,0 +1,17 @@
+package eventbus;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class EventSchedule {
+    HashMap<String , Boolean> hashMap ;
+
+    public HashMap<String, Boolean> getHashMap() {
+        return hashMap;
+    }
+
+    public void setHashMap(HashMap<String, Boolean> hashMap) {
+        this.hashMap = hashMap;
+    }
+
+}
